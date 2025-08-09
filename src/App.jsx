e98d0621c5aa2264609b3c06f6b9fa0682600f88
LiorKirshner <liorkk7@gmail.com>
2025-08-09 17:21:59 +0300

    first init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import NoteForm from "./components/NoteForm";
+
+function App() {
+  return <NoteForm />;
+}
+
+export default App;
---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,3 @@
+const NoteForm = function () {};
+
+export default NoteForm;
